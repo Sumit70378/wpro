a3023c68c29937214341547ec878934310e3aea7
Sumit70378 <127169450+Sumit70378@users.noreply.github.com>
2024-01-13 09:32:06 +0530

    Add files via upload

---
+++ clock2.js
@@ -0,0 +1,17 @@
+const http = require('node:http');
+const fs = require('fs');
+
+
+const hostname = '127.0.0.1';
+const port = 80;
+const filecontent = fs.readFileSync('clock.html' , 'utf-8');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', "text/html");
+  res.end(filecontent);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
